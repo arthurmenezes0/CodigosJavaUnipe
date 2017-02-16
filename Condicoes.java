b38225204074f1706fa667b16cb4746516c3c9c5
arthurmenezes0 <arthurmenezes0@gmail.com>
2017-02-16 19:19:53 -0300

    Add files via upload

---
+++ Condicoes.java
@@ -0,0 +1,20 @@
+package br.unipe.java.unidade1;
+
+import java.util.Scanner;
+
+public class Condicoes {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Informe um numero para ser testado");
+		int z = leitor.nextInt();
+		if (z == 10) {
+			System.out.println("z e igual a 10 :)");
+		} else {
+			System.out.println("z nao e igual a 10");
+		}
+		System.out.println("e isso é enrolação");
+		leitor.close();
+		
+	}
+}
