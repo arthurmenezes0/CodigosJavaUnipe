0a9940e028c73f29558d4cce18fc5395e8da6e48
arthurmenezes0 <arthurmenezes0@gmail.com>
2017-02-17 00:06:45 -0200

    Create canil.java

---
+++ canil.java
@@ -0,0 +1,21 @@
+
+public class Canil {
+
+	/**
+	 * Esse método realiza o alojamento
+	 * do animal para adoção ou exterminio.
+	 */
+
+	public void alojar(){
+		System.out.println("Alojando cachorro");
+	}
+  
+	/**
+	 * Esse método realiza o extermínio de cachorro
+	 * @param nome do animal que será exterminado
+	 */
+
+	public void exterminarCachorro(String nome){
+		System.out.println("Exterminando Cachorro");
+	}
+}
