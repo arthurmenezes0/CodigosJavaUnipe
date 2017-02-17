b830791c525bc0ae7fd90cf18fc59649c8399a6c
arthurmenezes0 <arthurmenezes0@gmail.com>
2017-02-16 23:55:00 -0300

    Add files via upload

---
+++ UsarCanil.java
@@ -0,0 +1,12 @@
+
+public class UsarCanil {
+
+	public static void main(String[] args) {		
+
+		Canil masmorra = new Canil();
+		
+		masmorra.alojar();
+		masmorra.exterminarCachorro("Snow");
+
+	}
+}
